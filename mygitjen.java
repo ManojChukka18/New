4c09e8b7c20d7917cc1f995b21c899085ad01ba0
root <root@ip-172-31-75-175.ec2.internal>
2025-12-28 13:39:04 +0000

    msg

---
+++ mygitjen.java
@@ -0,0 +1,9 @@
+class new{
+public static void main(String[] args){
+System.out.println("how are yiu");
+System.out.println("how are yiu");
+System.out.println("how are yiu");
+System.out.println("how are yiu");
+System.out.println("how are yiu");
+}
+}
