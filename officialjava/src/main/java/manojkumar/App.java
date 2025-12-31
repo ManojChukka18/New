6fcaae170bdcaf73102d9ba7bc895ecace135a76
root <root@ip-172-31-75-175.ec2.internal>
2025-12-31 08:35:44 +0000

    mem

---
+++ officialjava/src/main/java/manojkumar/App.java
@@ -0,0 +1,16 @@
+package manojkumar;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
